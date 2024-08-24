dfc1809446169834a9a1e220fdde31b5916fa987
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-24 00:05:16 -0300

    add-imagens-novas-tela-login-icone-app

--- components/Login/LogoLogin/LogoLogin.tsx
+++ components/Login/LogoLogin/LogoLogin.tsx
@@ -6,7 +6,7 @@ import { styles } from './styles';
 export function LogoLogin() {
   return (
     <View style={styles.container}>
-        <Image source={require("../../../assets/images/Login/security-gif.gif")} style={[styles.imagemLogin]}/>
+        <Image source={require("../../../assets/images/Login/icon-bem-mais.png")} style={[styles.imagemLogin]}/>
         <Text style={[styles.tituloLogo]}>Health App</Text>
     </View>
   );
